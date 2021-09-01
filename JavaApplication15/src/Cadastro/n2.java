0a534be1f72a565f0dadf7f93025cc7b4a7ba1a7
MAURICIO LUZ STOFFEL JUNIOR <mauricio.junior1@alunos.sc.senac.br>
2021-09-01 19:52:46 -0300

    coloquei os arquivos da aula 14

---
+++ JavaApplication15/src/Cadastro/n2.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Cadastro;
+
+/**
+ *
+ * @author mauricio.junior1
+ */
+public class n2 extends n1 {
+
+    public int numS = 1248941594;
+    public int valor = 399;
+}
